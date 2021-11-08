f9e846c62219bd6d9b06a7b126207a36cf35efc6
Albert Garcia F <alberd_beto@hotmail.com>
2021-11-08 12:16:20 +0100

    count button

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,23 @@ var botonElement = document.getElementById("botonClick");
             }
 
 
+//Pasar de botón Start a botón Click
+var startGame1 = document.getElementById("starButton");
+var clickGame1= document.getElementById("botonClick");
+        startGame1.onclick=function(){
+            startGame1.classList.add("hidden");
+            clickGame1.classList.remove("hidden");
+        }
+
+//boton que cuenta los clicks
+var botonElement = document.getElementById("botonClick");
+            var pElement = document.getElementById("areaContador");
+            var count = 0;
+            botonElement.onclick = function () {
+                count++;
+                pElement.textContent = count;
+            }
+
 /*cambio paginas*/
 
 
