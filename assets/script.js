699fa83addff5a957a9cde68110c30fe920bad27
Albert Garcia F <alberd_beto@hotmail.com>
2021-11-08 11:44:51 +0100

    switch pages

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+/*contador boton*/
+
 var botonElement = document.getElementById("botonClick");
             var pElement = document.getElementById("areaContador");
             var contador = 0;
@@ -5,3 +7,49 @@ var botonElement = document.getElementById("botonClick");
                 contador++;
                 pElement.textContent = contador;
             }
+
+
+/*cambio paginas*/
+
+
+const sectionPages = document.querySelector(".pages");
+
+const circulos = document.querySelectorAll('.circulo');
+const tabsy = document.querySelector(".tabsy");
+const anterior = document.getElementById('anterior');
+const siguiente = document.getElementById('siguiente');
+const tabby = document.querySelectorAll(".tabby");
+
+siguiente.addEventListener('click', () => {
+    for(let i = 0; i < tabsy.childElementCount - 1; i++){
+        if(tabsy.children[i].classList.contains("active")){
+            addRemoveClass(tabsy.children, i, "active", true)
+            addRemoveClass(tabby, i, "is-active", true)
+            anterior.disabled = false;
+            if(i+1 == tabsy.childElementCount-1) siguiente.disabled = true;
+            return;
+        }
+    }
+});
+
+anterior.addEventListener('click', () => {
+    for(let i = 0; i < tabsy.childElementCount; i++){
+        if(tabsy.children[i].classList.contains("active")){
+            addRemoveClass(tabsy.children, i, "active", false)
+            addRemoveClass(tabby, i, "is-active", false)
+            if(i <= 1) anterior.disabled = true;
+            if(i-1 <= tabsy.childElementCount) siguiente.disabled = false;
+            return;
+        }
+    }
+});
+
+function addRemoveClass(element, i, classToChange, isNext){
+  if(isNext){
+      element[i].classList.remove(classToChange);
+      element[i+1].classList.add(classToChange);
+  } else {
+      element[i].classList.remove(classToChange);
+      element[i-1].classList.add(classToChange);
+  }
+}
