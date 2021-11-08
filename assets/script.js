b43cc274bf8877cc40e5c31da2712ed1434c7d63
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-11-08 12:34:52 +0000

    Add game over function

--- assets/script.js
+++ assets/script.js
@@ -1,72 +1,73 @@
-/*contador boton*/
-
-var botonElement = document.getElementById("botonClick");
-            var pElement = document.getElementById("areaContador");
-            var count = 0;
-            botonElement.onclick = function () {
-                contador++;
-                pElement.textContent = contador;
-            }
-
 
 //Pasar de botón Start a botón Click
 var startGame1 = document.getElementById("starButton");
-var clickGame1= document.getElementById("botonClick");
-        startGame1.onclick=function(){
-            startGame1.classList.add("hidden");
-            clickGame1.classList.remove("hidden");
-        }
+var clickGame1 = document.getElementById("botonClick");
+startGame1.onclick = function () {
+    startGame1.classList.add("hidden");
+    clickGame1.classList.remove("hidden");
+};
 
 //boton que cuenta los clicks
-var botonElement = document.getElementById("botonClick");
-            var pElement = document.getElementById("areaContador");
-            var count = 0;
-            botonElement.onclick = function () {
-                count++;
-                pElement.textContent = count;
-            }
+var clickNum = document.getElementById("areaContador");
+var count = 0;
+var time;
+clickGame1.addEventListener("click", game1);
+function game1() {
+    count++;
+    clickNum.textContent = count;
+    time = setTimeout(gameOver, 3000); //Temporizador juego
+};
+
+// función fin juego
+var score= count;
+var sectionGame= document.getElementById("game1");
+var content=document.createTextNode("Game Over");
+function gameOver(){
+    clickGame1.removeEventListener("click", game1);
+    sectionGame.appendChild(content);
+    console.log(score + "gameOver");
+}
 
 /*cambio paginas*/
 
-
 const sectionPages = document.querySelector(".pages");
 
-const circulos = document.querySelectorAll('.circulo');
+const circulos = document.querySelectorAll(".circulo");
 const tabsy = document.querySelector(".tabsy");
-const anterior = document.getElementById('anterior');
-const siguiente = document.getElementById('siguiente');
+const anterior = document.getElementById("anterior");
+const siguiente = document.getElementById("siguiente");
 const tabby = document.querySelectorAll(".tabby");
 
-siguiente.addEventListener('click', () => {
-    for(let i = 0; i < tabsy.childElementCount - 1; i++){
-        if(tabsy.children[i].classList.contains("active")){
-            addRemoveClass(tabsy.children, i, "active", true)
-            addRemoveClass(tabby, i, "is-active", true)
+siguiente.addEventListener("click", () => {
+    for (let i = 0; i < tabsy.childElementCount - 1; i++) {
+        if (tabsy.children[i].classList.contains("active")) {
+            addRemoveClass(tabsy.children, i, "active", true);
+            addRemoveClass(tabby, i, "is-active", true);
             anterior.disabled = false;
-            if(i+1 == tabsy.childElementCount-1) siguiente.disabled = true;
+            if (i + 1 == tabsy.childElementCount - 1) siguiente.disabled = true;
             return;
         }
     }
 });
 
-anterior.addEventListener('click', () => {
-    for(let i = 0; i < tabsy.childElementCount; i++){
-        if(tabsy.children[i].classList.contains("active")){
-            addRemoveClass(tabsy.children, i, "active", false)
-            addRemoveClass(tabby, i, "is-active", false)
-            if(i <= 1) anterior.disabled = true;
-            if(i-1 <= tabsy.childElementCount) siguiente.disabled = false;
+anterior.addEventListener("click", () => {
+    for (let i = 0; i < tabsy.childElementCount; i++) {
+        if (tabsy.children[i].classList.contains("active")) {
+            addRemoveClass(tabsy.children, i, "active", false);
+            addRemoveClass(tabby, i, "is-active", false);
+            if (i <= 1) anterior.disabled = true;
+            if (i - 1 <= tabsy.childElementCount) siguiente.disabled = false;
             return;
         }
     }
 });
 
-function addRemoveClass(element, i, classToChange, isNext){
-  if(isNext){
-      element[i].classList.remove(classToChange);
-      element[i+1].classList.add(classToChange);
-  } else {
-      element[i].classList.remove(classToChange);
-      element[i-1].classList.add(classToChange);
-  }
+function addRemoveClass(element, i, classToChange, isNext) {
+    if (isNext) {
+        element[i].classList.remove(classToChange);
+        element[i + 1].classList.add(classToChange);
+    } else {
+        element[i].classList.remove(classToChange);
+        element[i - 1].classList.add(classToChange);
+    }
 }
