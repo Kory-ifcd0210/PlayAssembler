ea2f36b664d2dc89d1d53ca17b481dd25551619b
Albert Garcia F <alberd_beto@hotmail.com>
2021-11-08 15:44:42 +0100

    game 2

--- assets/script.js
+++ assets/script.js
@@ -87,3 +87,21 @@ function addRemoveClass(element, i, classToChange, isNext) {
         element[i - 1].classList.add(classToChange);
     }
 }
+
+/*game 2*/ 
+
+
+function moveElmRand(elm){ 
+    elm.style.position ='absolute'; 
+    elm.style.top = Math.floor(Math.random()*90+5)+'%'; 
+    elm.style.left = Math.floor(Math.random()*90+5)+'%'; 
+   } 
+    
+   //get the #btn_test 
+   var btn_test = document.querySelector('#btn_test'); 
+    
+   //register to call moveElmRand() on mouseenter event to #btn_test 
+   btn_test.addEventListener('mouseenter', function(e){ moveElmRand(e.target);}); 
+    
+   //register click to #btn_test 
+   btn_test.addEventListener('click', function(e){ alert('You are Good.');});
