8df7e479fac45d57b66ee71fb741b4a95443ccdc
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-11-11 14:15:20 +0000

    iconos en score

--- assets/script.js
+++ assets/script.js
@@ -35,7 +35,7 @@ function chargeData(){
     for(var i in dataFormated)
     {
         gamers.innerHTML =
-        gamers.innerHTML + "<li>" + dataFormated[i].name   +"-"+dataFormated[i].score+ "</li>";
+        gamers.innerHTML + "<li>" +"<div class='icon'>" + "<i class='fas fa-cloud'></i>"+ "</div>"+ "<div class='playerData'>"+dataFormated[i].name   +"-"+dataFormated[i].score+"</div>"+ "</li>";
         displayScore.push(dataFormated[i]);
     }
     lengthy();
